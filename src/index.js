7f8cbd7707524a96409def5984eb41a05ccbc9e0
Jack Lach <jack.lach4@gmail.com>
2023-02-24 19:41:43 -0500

    populate dog-bar with pups

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@
 const init = () => {
     let pupDataUrl = 'http://localhost:3000/pups';
     let dogBar = document.querySelector('#dog-bar');
-    
+    let dogInfo = document.querySelector('#dog-info');
+
     fetch(pupDataUrl)
     .then(res => res.json())
-        .then(obj => {
-            console.log(obj);
+        .then(arr => {
+            console.log(arr);
+            for (const obj of arr){
+                let span = document.createElement('span');
+                span.textContent = obj.name;
+                span.addEventListener('click', displayPupInfo);
+                dogBar.appendChild(span);
+            }
         });
+    
+    function displayPupInfo(){
+        console.log('i have been clicked');
+    };
 };
 
 document.addEventListener('DOMContentLoaded', init);
